35d4c37e4a5fd6b2c5b1b85f29ea1e7a951aefd7
Jahid Hasan <teddyteem@gmail.com>
2022-07-20 16:59:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 
-const axios = require("axios").default;
-const arg = process.argv;
+const execSync = require("child_process").execSync;
+const path = require("path");
 
+execSync("mkdir src", { pwd: __dirname });
+execSync("mkdir src/controllers");
+execSync("mkdir src/models");
+execSync("mkdir src/variables");
+execSync("touch src/controllers/healthController.js");
+execSync("touch src/index.js");
+execSync("touch src/routes.js");
+execSync("touch src/.env");
+execSync("touch .gitignore");
+execSync("npm init -y", { pwd: path.join(__dirname, "src") });
+execSync("npm i axios", { pwd: path.join(__dirname, "src") });
+const axios = require("axios").default;
 const getData = async () => {
   return axios
     .get(`https://jsonplaceholder.typicode.com/todos/1`)
@@ -13,6 +25,5 @@ const getData = async () => {
 const printData = async () => {
   const data = await getData();
   console.log("=>>>>>>", data);
-  console.log("==================>Arg: ",arg);
 };
 printData();
