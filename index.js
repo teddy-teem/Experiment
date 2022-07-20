31f5da00d5d9d76926b0feb2a378ec05a25152c6
Jahid Hasan <teddyteem@gmail.com>
2022-07-21 00:16:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,3 @@ execSync(`touch ${arg[2]}/src/routes.js`);
 execSync(`touch ${arg[2]}/src/.env`);
 execSync(`touch ${arg[2]}/.gitignore`);
 execSync(`npm init -y`, { pwd: path.join(__dirname, `${arg[2]}/src`) });
-execSync(`npm i axios`, { pwd: path.join(__dirname, `${arg[2]}/src`) });
-
-const axios = require("axios").default;
-const getData = async () => {
-  return axios
-    .get(`https://jsonplaceholder.typicode.com/todos/1`)
-    .then(res => res.data)
-    .catch(err => err);
-};
-
-const printData = async () => {
-  const data = await getData();
-  console.log("=>>>>>>", data);
-};
-printData();
