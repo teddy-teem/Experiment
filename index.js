77f919014dd35ca5df02c77172209f868a01351d
Jahid Hasan <teddyteem@gmail.com>
2022-07-20 17:08:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 const execSync = require("child_process").execSync;
 const path = require("path");
+const arg = process.argv;
+execSync(`mkdir ${arg[2]}`, { pwd: __dirname });
+execSync(`mkdir ${arg[2]}/src`, { pwd: __dirname });
+execSync(`mkdir ${arg[2]}/src/controllers`);
+execSync(`mkdir ${arg[2]}/src/models`);
+execSync(`mkdir ${arg[2]}/src/variables`);
+execSync(`touch ${arg[2]}/src/controllers/healthController.js`);
+execSync(`touch ${arg[2]}/src/index.js`);
+execSync(`touch ${arg[2]}/src/routes.js`);
+execSync(`touch ${arg[2]}/src/.env`);
+execSync(`touch ${arg[2]}/.gitignore`);
+execSync(`npm init -y`, { pwd: path.join(__dirname, `${arg[2]}/src`) });
+execSync(`npm i axios`, { pwd: path.join(__dirname, `${arg[2]}/src`) });
 
-execSync("mkdir src", { pwd: __dirname });
-execSync("mkdir src/controllers");
-execSync("mkdir src/models");
-execSync("mkdir src/variables");
-execSync("touch src/controllers/healthController.js");
-execSync("touch src/index.js");
-execSync("touch src/routes.js");
-execSync("touch src/.env");
-execSync("touch .gitignore");
-execSync("npm init -y", { pwd: path.join(__dirname, "src") });
-execSync("npm i axios", { pwd: path.join(__dirname, "src") });
 const axios = require("axios").default;
 const getData = async () => {
   return axios
