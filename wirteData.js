bda3466d8854609a4bba12c399a99be4b7bcbb72
Jahid Hasan <jahid.hasan@pmamediagroup.com>
2022-07-23 01:09:03 +0600

    ready file added

---
+++ wirteData.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const temp = require("./temp.json");
+
+exports.writeController = (repo)=>{
+    fs.writeFileSync(`${repo}`, temp.healthController, );
+}
+exports.writeIndex = (repo)=>{
+    fs.writeFileSync(`${repo}`, temp.index );
+}
+exports.writeEnv = (repo, fileName)=>{
+    fs.writeFileSync(`${repo}`, temp.env+fileName );
+}
+exports.writeRoute = (repo)=>{
+    fs.writeFileSync(`${repo}`, temp.route );
+}
+exports.writeVarIndex = (repo)=>{
+    fs.writeFileSync(`${repo}`, temp.varIndex );
+}
+
+exports.writeGitignore = (repo)=>{
+    const string = fs.readFileSync("./gitignore.txt")
+    fs.writeFileSync(`${repo}`, string );
+}
