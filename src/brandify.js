3178e0ab7ad2bdde64783130377dfc51deff3375
jhaucorp <jahid.hasan@pmamediagroup.com>
2023-03-14 10:40:26 +0600

    Version Update

--- src/brandify.js
+++ src/brandify.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const shell = require("shelljs");
 const brandConfig = require("./brand-config.json");
+const environment = process.env.REACT_APP_ENVIRONMENT || "dev";
+const brandConfig = require(`./brand-config.${environment}.json`);
 const planBenefits = require("./planBenefits.json");
 const { colors, colorsV2, brandInfo, features, emails, defaults } = brandConfig;
 const hasBorder = colors.loginBorder !== "#ffffff";
